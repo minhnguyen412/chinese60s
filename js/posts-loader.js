fe3f7e48723f52d91caf1d5e5da4f17960d78ffb
minhnguyen412 <142096937+minhnguyen412@users.noreply.github.com>
2025-01-14 21:01:27 +0700

    Update posts-loader.js

--- js/posts-loader.js
+++ js/posts-loader.js
@@ -88,8 +88,8 @@ function showImageCard(imageData) {
 // Hàm để đóng image card
 function closeImageCard() {
     if (activeImageCard) {
-        activeImageCard.style.display = 'none'; // Đóng card
-        activeImageCard = null; // Reset activeImageCard
+        document.body.removeChild(activeImageCard); // Xóa card khỏi DOM
+        activeImageCard = null;
     }
 }
 
