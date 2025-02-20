92df1131d8d0ba7a66e773c3b4d6c09fde11b1e2
minhnguyen412 <142096937+minhnguyen412@users.noreply.github.com>
2025-02-20 08:26:08 +0700

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -182,11 +182,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     resultDiv.textContent = 'Incorrect. Try again!';
                     resultDiv.classList.add('incorrect');
                     incorrectAudio.play();
-                    // Xóa nút retry nếu có
-                    const retryButton = document.querySelector('.retry');
-                    if (retryButton) {
-                        retryButton.remove();
-                    }
+                    
                 }
             }
 
